fec76b8f9a73b6b76cf3bf18cef4d0422888c26b
Julie J <jeannet.julie@gmail.com>
2022-12-05 18:20:05 +0100

    Login Signout + setSaved setLike in Firestore + Display savedShows in user account

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,31 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+//   appId: process.env.REACT_APP_APP_ID
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyC1cR7B-cB-uhk9QfMua6BSEDzIIKTde24",
+  authDomain: "react-netflix-2eb3c.firebaseapp.com",
+  projectId: "react-netflix-2eb3c",
+  storageBucket: "react-netflix-2eb3c.appspot.com",
+  messagingSenderId: "847525021915",
+  appId: "847525021915:web:95346d0ea11f0ac82ba4fe"
 };
+
+// REACT_APP_FIREBASE_API_KEY=AIzaSyC1cR7B-cB-uhk9QfMua6BSEDzIIKTde24
+// REACT_APP_FIREBASE_AUTH_DOMAIN=react-netflix-2eb3c.firebaseapp.com
+// REACT_APP_FIREBASE_PROJECT_ID=react-netflix-2eb3c
+// REACT_APP_FIREBASE_STORAGE_BUCKET=react-netflix-2eb3c.appspot.com
+// REACT_APP_MESSAGIN_SENDER=847525021915
+// REACT_APP_APP_ID=1:847525021915:web:95346d0ea11f0ac82ba4fe
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
